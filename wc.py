2620a2a322462845e29a3dd51b41659ad8720676
Le Huy <lehuy20@gmail.com>
2011-12-10 17:05:18 +0100

    update docs

--- wc.py
+++ wc.py
@@ -3,7 +3,7 @@ import os
 total=0
 for top, dirs, files in os.walk('./'):
   for name in files:       
-    if os.path.splitext(name)[1]==".py":
+    if os.path.splitext(name)[1] in [".py",".textile",".yaml"]:
       fullpath=os.path.join(top, name)
       with open(fullpath) as f:
         fl = sum([1 for line in f])
